99791f34e588fe319ffa6194c7c432fefb3ff2a7
Ian <itrueba0809@gmail.com>
2017-10-07 12:13:29 +0200

    Working on enemy anims

--- BGW_JAM_Cripplo_team/Assets/Scripts/Enemy_Behaviour.cs
+++ BGW_JAM_Cripplo_team/Assets/Scripts/Enemy_Behaviour.cs
@@ -9,9 +9,28 @@ public enum Enemy_Type
     ENEMY_RANDOM
 }
 
-public class Enemy_Behaviour : MonoBehaviour {
+public enum Enemy_direction
+{
+    E_RIGHT,
+    E_LEFT,
+    E_UP,
+    E_DOWN,
+}
+
+public enum Enemy_state
+{
+    E_IDLE,
+    E_WALK,
+    E_HAPPY,
+}
+
+public class Enemy_Behaviour : MonoBehaviour
+{
 
     public Enemy_Type e_type;
+    public Enemy_direction e_dir_state;
+    public Enemy_state e_state = Enemy_state.E_IDLE;
+
     public float e_speed = 1.0f;
     public float e_max_distance = 0;
     public float e_target_min_distance = 0.2f;
@@ -20,30 +39,39 @@ public class Enemy_Behaviour : MonoBehaviour {
     Vector3 e_start_position;
     Vector3 e_target_position;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         ChangeObjective();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
+        DirectionState();
         Movement();
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if(coll.tag == "smilerang")
+        if (e_state != Enemy_state.E_HAPPY)
         {
-            //TODO: temporal measure
-            Destroy(gameObject);
+            if (coll.tag == "smilerang")
+            {
+                //TODO: temporal measure
+                StateMachine(Enemy_state.E_HAPPY);
+
+                gameObject.GetComponent<Collider2D>().isTrigger = true;
+
+                //Count enemy to win
+            }
         }
-        
+
     }
+
     void Movement()
     {
-        switch(e_type)
+        switch (e_type)
         {
             case Enemy_Type.ENEMY_STATIC:
                 break;
@@ -58,7 +86,7 @@ public class Enemy_Behaviour : MonoBehaviour {
     }
 
     void MovementKinematic()
-    {   
+    {
         transform.position += e_dir * e_speed;
 
         Vector3 distance = e_target_position - transform.position;
@@ -78,7 +106,7 @@ public class Enemy_Behaviour : MonoBehaviour {
         if (distance.magnitude < e_target_min_distance)
         {
             int rand = Random.Range(0, 3);
-            switch(rand)
+            switch (rand)
             {
                 case 0:
                     e_dir = new Vector3(-1.0f, 0.0f);
@@ -102,7 +130,49 @@ public class Enemy_Behaviour : MonoBehaviour {
 
     void ChangeObjective()
     {
+        StateMachine(Enemy_state.E_WALK);
         e_start_position = transform.position;
         e_target_position = e_start_position + e_dir * e_max_distance;
+    }
+
+
+    void DirectionState()
+    {
+        if (e_dir != Vector3.zero)
+        {
+            float dir_angle = Mathf.Atan2(e_dir.y, e_dir.x) * Mathf.Rad2Deg;
+
+
+            if (dir_angle < -135.0f || dir_angle >= 135.0f)
+            {
+                //RIGHT
+                e_dir_state = Enemy_direction.E_RIGHT;
+            }
+            else if (dir_angle < 45.0F && dir_angle >= -45.0f)
+            {
+                //LEFT
+                e_dir_state = Enemy_direction.E_RIGHT;
+            }
+            else if (dir_angle < 135.0f && dir_angle >= 45.0f)
+            {
+                //DOWN
+                e_dir_state = Enemy_direction.E_RIGHT;
+            }
+            else if (dir_angle < -45.0f && dir_angle >= -135.0f)
+            {
+                //UP
+                e_dir_state = Enemy_direction.E_RIGHT;
+            }
+        }
+        else
+        {
+            e_dir_state = Enemy_direction.E_RIGHT;
+        }
+
+    }
+
+    public void StateMachine(Enemy_state state)
+    {
+        e_state = state;
     }
 }
